6d783a230fa6a9b00f631a82d0a35801f1a665ec
Mar Bocatcat <mar.bocatcat@gmail.com>
2019-03-09 14:34:34 -0600

    installed SQL_ALCHEMY and added DB User and Post class Model.

--- main.py
+++ main.py
@@ -1,14 +1,73 @@
 #!/bin/env python3
 
+from datetime import datetime 
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Set SQLALCHEMY Instance Location to our site project location.
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
+# Set SQLALCHEMY Instance 
+db = SQLAlchemy(app)
+
+# Create SQLAlchemy model/class for both User and the Post.
+
+
+class User(db.Model):
+    # Create db id column and set db as integer, primary_key sets the id as unique for each user.
+    id = db.Column(db.Integer, primary_key=True)
+    # Create db username and set the db as string with max length set to 20 charachters , set the username to unique, and nullable meands it cannot be empty.
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    # Create db for email with same setting as username. 
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    # Create db for image file and set default image.
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    # Create db for password.
+    password = db.Column(db.String(60), nullable=False)
+    # Reference the author from the post class model.
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    # Creates a class and returns itself.
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    # Create Unique ID for the post.
+    id = db.Column(db.Integer, primary_key=True)
+    # Create title of the post max of 100.
+    title = db.Column(db.String(100), nullable=False)
+    # Add the date posted to default datetime.
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    # Add the content.
+    content = db.Column(db.Text, nullable=False)
+    # Id of the user that authored the post.
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+
+posts = [
+        {
+            
+            "title": "Python",
+            "content": "An Open Source Project that Defined my life",
+            "date_posted": "March 9, 2019",
+            },
+        {
+            "title": "You cant Rust that!",
+            "content": "Some tips on how to be more productive in Rust",
+            "date_posted": "March 19, 2019", 
+            }
+        ]
 
 
 @app.route("/")
 @app.route("/home")
 def main():
-    return render_template('landing.html', title="Blog | Mar Bocatcat")
+    return render_template('landing.html', title="Blog | Mar Bocatcat", posts=posts)
 
 
 if __name__ == "__main__":
